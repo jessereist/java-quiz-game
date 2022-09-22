022ada6a934a199c1475319902b98a67a3d077f0
jessereist <jessereist@gmail.com>
2022-09-22 04:59:40 -0500

    v1.0! Cleaned code a bit. More features to come!

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args)  {
-        //Write main execution code here
         String playerName;
         Scanner scn = new Scanner(System.in);
         System.out.println("Hello! Please enter your name...");
